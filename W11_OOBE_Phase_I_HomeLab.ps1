fc4a243298fb90440b6a60491e00916e5a642a9b
SgtJericho <68450749+SgtJericho@users.noreply.github.com>
2024-06-12 14:09:56 -0400

    Add files via upload

---
+++ W11_OOBE_Phase_I_HomeLab.ps1
@@ -0,0 +1,21 @@
+﻿# Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned
+# Created by Jason Prespolis | 5:57 PM 5/25/2024 | for Windows 10/11 Pro / Enterprise
+
+Date
+
+.\Install_Power_Plan.ps1
+.\UpdateOS.ps1
+.\UpdateDrivers.ps1
+.\W11_Local_Packages.bat
+.\W11_Install_Lenovo_Apps.ps1
+.\W11_Install_Standard_Package.ps1
+.\W11_Install_Registry_Edits.bat
+.\W11_Remove_Ads_Settings
+.\W11_Install_PrivacySettings.ps1
+.\W11_Install_Features.ps1
+.\W11_Remove_Ads_Settings.bat
+.\W11_Remove_AppxPackage_Debloat.ps1
+.\Clear_Evnt.bat
+
+Date
+Shutdown -r -t 900
