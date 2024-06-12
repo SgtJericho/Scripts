389ab3bfe4200516fca95a46e7696c39dbca0971
SgtJericho <68450749+SgtJericho@users.noreply.github.com>
2024-06-12 12:02:57 -0400

    Add files via upload
    This command "ms-availablenetworks provider" Starts wireless network provider for windows.
    
    This part of the command starts a new PowerShell process with specific options:
    
    -NoProfile: Prevents loading of the user profile.
    -NoExit: Keeps the PowerShell window open after executing the command.
    Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned:
    Sets the execution policy for the current session to RemoteSigned.
    
    The RemoteSigned execution policy allows running local scripts (signed by a trusted publisher) and remote scripts (not signed) after prompting for confirmation.

---
+++ RemoteSigned_Wifi.bat
@@ -0,0 +1,4 @@
+@Echo off
+:: Created by Jason Prespolis | 5:57 PM 5/25/2024 | for Windows 10/11 Enterprise
+Start ms-availablenetworks:
+Start PowerShell.exe -NoProfile -NoExit Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned
