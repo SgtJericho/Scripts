7d21f237d2883c63847156ff7d9dfb7df6a6b2c9
SgtJericho <68450749+SgtJericho@users.noreply.github.com>
2024-06-12 12:17:57 -0400

    Add files via upload
    This command runs a PowerShell script named UpdateDrivers.ps1 
    
    Outdated drivers can cause your Windows devices to run slowly and impact their overall performance. Driver updates can enhance the user experience by fixing bugs and other security issues associated with the older version. The PowerShell script below will download and install any pending driver updates on your Windows device. Using Hexnode’s Execute Custom Script action, you can remotely run custom scripts on your Windows devices without any manual intervention.
    
    Lenovo System Update is a utility provided by Lenovo for managing system updates, including driver updates, BIOS updates, and software patches.
    Running this command will launch the Lenovo System Update application.
    C:\Program Files (x86)\Lenovo\System Update\tvsu.exe”
    
    The Device Manager allows you to view and manage hardware devices connected to your computer. You can update drivers, disable devices, and troubleshoot issues related to hardware components.  Start devmgmt.msc:
    
    This command opens the Windows Update settings page directly.  It allows you to check for updates, install available updates, and configure update settings.
    start ms-settings:windowsupdate:

---
+++ UpdateDrivers.bat
@@ -0,0 +1,6 @@
+:: Created by Jason Prespolis | 5:57 PM 5/25/2024 | for Windows 10/11 Enterprise
+Start ms-availablenetworks:
+Start devmgmt.msc
+start ms-settings:windowsupdate
+"C:\Program Files (x86)\Lenovo\System Update\tvsu.exe"
+PowerShell.exe -NoProfile -NoExit -ExecutionPolicy Bypass .\UpdateDrivers.ps1
