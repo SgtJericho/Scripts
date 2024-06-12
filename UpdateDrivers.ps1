4b2ae010d1339488d9a37236ccedf6a6d9809763
SgtJericho <68450749+SgtJericho@users.noreply.github.com>
2024-06-12 14:03:42 -0400

    Update UpdateDrivers.ps1

--- UpdateDrivers.ps1
+++ UpdateDrivers.ps1
@@ -1,7 +1,6 @@
 ﻿# Check for available updates
 # Created by Jason Prespolis | 6:33 PM 5/11/2024 | for Windows 11 Enterprise
 
-# https://www.hexnode.com/mobile-device-management/help/powershell-script-to-update-drivers-on-windows-devices/
 $UpdateSvc = New-Object -ComObject Microsoft.Update.ServiceManager
 $UpdateSvc.AddService2("7971f918-a847-4430-9279-4a52d1efe18d",7,"")
 $Session = New-Object -ComObject Microsoft.Update.Session
