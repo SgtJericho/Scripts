389c244115a64a18d79bb1e4d2baae3cc8a9d590
SgtJericho <68450749+SgtJericho@users.noreply.github.com>
2024-06-12 17:25:53 -0400

    Update W11_Install_Lenovo_Apps.ps1

--- W11_Install_Lenovo_Apps.ps1
+++ W11_Install_Lenovo_Apps.ps1
@@ -3,7 +3,7 @@
 # Created by Jason Prespolis | 5:57 PM 5/25/2024 | for Windows 10/11 Enterprise
 
 # Define download URLs
-# $commercialVantageUrl = "https://download.lenovo.com/pccbbs/thinkvantage_en/metroapps/Vantage/LenovoCommercialVantage_10.2401.29.0.zip"
+$commercialVantageUrl = "https://download.lenovo.com/pccbbs/thinkvantage_en/metroapps/Vantage/LenovoCommercialVantage_10.2401.29.0.zip"
 $systemUpdateUrl = "https://download.lenovo.com/pccbbs/thinkvantage_en/system_update_5.08.02.25.exe"
 $diagnosticsUrl = "https://download.lenovo.com/pccbbs/thinkvantage_en/ldiag_5.11.0_windows_x64.exe"
 
@@ -37,8 +37,8 @@ Start-Process -Wait -FilePath "$downloadsPath\ldiag_5.11.0_windows_x64.exe" -Arg
 #################
 
 # Define the URL and the destination paths
-# $url = "https://download.lenovo.com/pccbbs/thinkvantage_en/metroapps/Vantage/LenovoCommercialVantage_10.2401.29.0.zip"
-# $destinationZip = "$env:USERPROFILE\Downloads\LenovoCommercialVantage_10.2401.29.0.zip"
+$url = "https://download.lenovo.com/pccbbs/thinkvantage_en/metroapps/Vantage/LenovoCommercialVantage_10.2401.29.0.zip"
+$destinationZip = "$env:USERPROFILE\Downloads\LenovoCommercialVantage_10.2401.29.0.zip"
 $extractPath = "$env:USERPROFILE\Downloads\extracted"
 
 # Download the file
